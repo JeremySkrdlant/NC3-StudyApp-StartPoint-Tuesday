fd2089f122ebdfbdcd4bab6a9c573df4d75c61cf
Jeremy <jeremy.skrdlant@nwktc.edu>
2025-01-09 11:03:27 -0700

    Initial Commit

---
+++ Study Application/Study_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Study_ApplicationApp.swift
+//  Study Application
+//
+//  Created by admin on 1/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Study_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Study ApplicationTests/Study_ApplicationTests.swift
@@ -0,0 +1,17 @@
+//
+//  Study_ApplicationTests.swift
+//  Study ApplicationTests
+//
+//  Created by admin on 1/9/25.
+//
+
+import Testing
+@testable import Study_Application
+
+struct Study_ApplicationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
